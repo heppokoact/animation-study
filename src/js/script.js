aa12434bbc4baf900ac660d5097de212aacd6cb7
yoshidacojp <yoshidacojp@gmail.com>
2020-03-09 09:48:12 +0900

    transitionの練習

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,8 @@
-console.log(7);
+$(() => {
+  let logArea = $("#boxTransitionLogArea");
+  let log = e => {
+    logArea.append(`<li>${e.type}, ${e.originalEvent.propertyName}</li>`);
+  };
+  $(".box").on("transitionstart", log);
+  $(".box").on("transitionend", log);
+});
